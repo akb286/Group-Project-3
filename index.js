42040c83f0e4bfc505d6986dc9e58ab4cf82487e
Alicia <32777002+akb286@users.noreply.github.com>
2024-05-06 19:37:14 -0600

    first push!
    first push!

---
+++ index.js
@@ -0,0 +1,9 @@
+// MENU FUNCTION
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
